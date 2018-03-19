7497bc881ccb98e9df78724812308adea9154d5b
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-03-19 10:50:27 +0100

    Revert "Make all titles optional, add file."
    This reverts commit 6486c60209fb5bff76b437775d433e483be55a88.

--- src/kitconcept/richpage/content/googlemap.py
+++ src/kitconcept/richpage/content/googlemap.py
@@ -10,7 +10,7 @@ class IGoogleMap(Interface):
 
     title = schema.TextLine(
         title=_(u"Title"),
-        required=False,
+        required=True,
     )
 
     google_map_embed_url = schema.TextLine(
