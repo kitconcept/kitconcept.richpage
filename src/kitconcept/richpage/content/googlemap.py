d76488549876c2c4d98d7ff32d3cc94207ebd3d7
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-03-23 13:15:33 +0100

    Change GoogleMaps description.

--- src/kitconcept/richpage/content/googlemap.py
+++ src/kitconcept/richpage/content/googlemap.py
@@ -15,14 +15,9 @@ class IGoogleMap(Interface):
 
     google_map_embed_url = schema.TextLine(
         title=_(u"Google Map embed URL"),
-        description=_(u"""Wie bekomme ich die Google Map URL?
-1.Öffnen Sie Google Maps.
-2.Wenn Sie eine Karte, Street View-Bilder oder eine Route mit anderen teilen
-möchten, müssen sie in der aktuellen Kartenansicht zu sehen sein.
-3.Klicken Sie zum Teilen auf das Menü .
-4.Wählen Sie Karte teilen oder einbetten aus. Wenn Sie diese Option nicht
-sehen, klicken Sie stattdessen auf Link zu aktueller Karte.
-5.Kopiere den Link und füge ihn in dieses Feld ein."""),
+        description=_(u"""1) Auf GoogleMaps gehen und in das Suchfeld eine Adresse eingeben.
+2) Auf das Teilen-Symbol klicken und dann auf "Karte einbetten" gehen.
+3) Es öffnet sich ein iframe-Code. Hier den Teil hinter src= zwischen den Anführungszeichen kopieren und hier einfügen."""), # noqa
         required=True,
     )
 
