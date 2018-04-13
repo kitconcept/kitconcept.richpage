2826152761c976a3d3d2d76410571156ecbcb2e4
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-04-13 10:34:22 +0200

    Set transcription for fields for videos

--- src/kitconcept/richpage/content/video.py
+++ src/kitconcept/richpage/content/video.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from kitconcept.richpage import _
-from zope import schema
-from zope.interface import Interface
 from plone.dexterity.content import Item
+from plone.namedfile import field as namedfile
+from zope import schema
 from zope.interface import implementer
+from zope.interface import Interface
 
 
 class IVideo(Interface):
@@ -25,6 +26,21 @@ class IVideo(Interface):
         description=_(
             u"Sie können entweder eine Youtube video URL oder eine Playlist hinzufügen"  # noqa
         ),
+        required=True,
+    )
+
+    transcript_description = schema.Text(
+        title=_(u"Transkript Beschreibung"),
+        required=False,
+    )
+
+    transcript_title = schema.TextLine(
+        title=_(u"Transkript Titel"),
+        required=False,
+    )
+
+    transcript_file = namedfile.NamedBlobFile(
+        title=_(u'Transkript Datei'),
         required=True,
     )
 
