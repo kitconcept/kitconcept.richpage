d80ce0dd4188f1b6eaeed7e8ed09091f95c0da01
Rob Gietema <rob.gietema@gmail.com>
2018-07-24 10:20:01 +0200

    Added image align.

--- src/kitconcept/richpage/content/image.py
+++ src/kitconcept/richpage/content/image.py
@@ -4,6 +4,12 @@ from zope import schema
 from zope.interface import Interface
 from plone.dexterity.content import Item
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+align = SimpleVocabulary(
+    [SimpleTerm(value=u'left', title=_(u'Left')),
+     SimpleTerm(value=u'right', title=_(u'Right'))]
+    )
 
 
 class IImage(Interface):
@@ -12,6 +18,12 @@ class IImage(Interface):
         title=_(u"Title"),
         required=False,
     )
+
+    align = schema.Choice(
+            title=_(u"Align"),
+            vocabulary=align,
+            required=False,
+        )
 
 
 @implementer(IImage)
