cb6c96e9888caabb58b2f864c3fda5d1def27510
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-02-20 11:41:03 +0100

    Back to development: 1.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='kitconcept.richpage',
-    version='1.1.0',
+    version='1.1.1.dev0',
     description="Folderish page with rich content",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
